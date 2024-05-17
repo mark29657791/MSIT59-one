42d7df7d74d51c523ca1d79ec06c66e47746e27d
mark29657791 <markhuang0707@gmail.com>
2024-05-17 16:09:54 +0800

    增加物鍵

--- 行程.js
+++ 行程.js
@@ -1,10 +1,60 @@
-$(function () {
-    $("#datepicker").datepicker({
-      showAnim: "clip"
+// 行程.js
+//日期選擇
+$( function() {
+  var dateFormat = "mm/dd/yy",
+    from = $( "#from" )
+      .datepicker({
+        defaultDate: "+1w",
+        changeMonth: true,
+        numberOfMonths: 1
+      })
+      .on( "change", function() {
+        to.datepicker( "option", "minDate", getDate( this ) );
+      }),
+    to = $( "#to" ).datepicker({
+      defaultDate: "+1w",
+      changeMonth: true,
+      numberOfMonths: 1
+    })
+    .on( "change", function() {
+      from.datepicker( "option", "maxDate", getDate( this ) );
     });
-    displaySelectedOptions(); 
-  });
+
+  function getDate( element ) {
+    var date;
+    try {
+      date = $.datepicker.parseDate( dateFormat, element.value );
+    } catch( error ) {
+      date = null;
+    }
+
+    return date;
+  }
+} );
+
+
   
+$(document).ready(function () {
+  $('#themeSelect').change(function () {
+    var days = $(this).val();
+    $('#itinerary1Select, #itinerary2Select, #itinerary3Select').addClass('hidden');
+
+    if (days >= 1) {
+      $('#itinerary1Select').removeClass('hidden');
+    }
+    if (days >= 2) {
+      $('#itinerary2Select').removeClass('hidden');
+    }
+    if (days == 3) {
+      $('#itinerary3Select').removeClass('hidden');
+    }
+  });
+
+  // Initialize datepicker
+  $('#datepicker').datepicker({
+    dateFormat: 'yy-mm-dd'
+  });
+});
   function displaySelectedOptions() {
     var location = $('#locationSelect option:selected').text();
     var theme = $('#themeSelect option:selected').text();
@@ -25,13 +75,14 @@ $(function () {
     var itinerary2 = $('#itinerary2Select option:selected').text();
     var itinerary3 = $('#itinerary3Select option:selected').text();
   
-    var output = '<h4>您選擇的行程:</h4>';
+    var output = '<br><h3>您選擇的行程:</h3><br>';
     output += '<p>地點: ' + location + '</p>';
     output += '<p>出遊天數: ' + theme + '</p>';
-    output += '<p>出發日期: ' + $('#datepicker').val() + '</p>';
+    output += '<p>出發日期: ' + $('#from').val() + '</p>';
+    output += '<p>結束日期: ' + $('#to').val() + '</p>';
     output += '<p>第一天: ' + itinerary1 + '</p>';
     output += '<p>第二天: ' + itinerary2 + '</p>';
-    output += '<p>第三天: ' + itinerary3 + '</p>';
+    output += '<p>第三天: ' + itinerary3 + '</p> <br>';
   
     $('#outputDiv').html(output);
     $(function () {
