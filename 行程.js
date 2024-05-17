583c0029ab7d1826de9e67d91cabb2cabfc9e68d
mark29657791 <markhuang0707@gmail.com>
2024-05-17 10:04:55 +0800

    517

--- 行程.js
+++ 行程.js
@@ -2,7 +2,7 @@ $(function () {
     $("#datepicker").datepicker({
       showAnim: "clip"
     });
-    displaySelectedOptions(); // 初次載入時顯示選項內容
+    displaySelectedOptions(); // 日曆選擇時顯示選項內容
   });
   
   function displaySelectedOptions() {
@@ -15,7 +15,7 @@ $(function () {
     $('#locationSelect, #themeSelect, #itinerary1Select, #itinerary2Select, #itinerary3Select').change(function () {
       displaySelectedOptions();
     });
-    displaySelectedOptions(); // 初次載入時顯示選項內容
+    displaySelectedOptions(); // 選擇時顯示選項內容
   });
   
   function displaySelectedOptions() {
@@ -27,18 +27,18 @@ $(function () {
   
     var output = '<h4>您選擇的行程:</h4>';
     output += '<p>地點: ' + location + '</p>';
-    output += '<p>特色主題: ' + theme + '</p>';
-    output += '<p>日期: ' + $('#datepicker').val() + '</p>';
+    output += '<p>出遊天數: ' + theme + '</p>';
+    output += '<p>出發日期: ' + $('#datepicker').val() + '</p>';
     output += '<p>早上行程: ' + itinerary1 + '</p>';
     output += '<p>下午行程: ' + itinerary2 + '</p>';
-    output += '<p>晚上行程: ' + itinerary3 + '</p>';
+    output += '<p>晚上行程: ' + itinerary3 + '</p>';// 新增
   
     $('#outputDiv').html(output);
     $(function () {
         $("#datepicker").datepicker({
           showAnim: "clip"
         });
-        displaySelectedOptions(); // 初次載入時顯示選項內容
+        displaySelectedOptions(); // 日曆選擇時顯示選項內容
       });
   }
   
